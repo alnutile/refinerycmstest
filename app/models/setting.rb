8b3fe6075072d359409be37fcb620760778e063c
Alfred Nutile <alfrednutile@gmail.com>
2013-07-11 12:05:14 -0400

    Missing image

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,5 @@
 class Setting < ActiveRecord::Base
   attr_accessible :color_background, :color_footer, :color_top, :footer, :logo
+
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
