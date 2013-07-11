70768c3af0b863efe4edc05303e4515fcda7c5b5
Alfred Nutile <alfrednutile@gmail.com>
2013-07-11 11:45:04 -0400

    Colors working as far as effecting the page as the user turns the dial

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,8 +1,21 @@
 (function() {
 	$('#top, #background, #footer').colorpicker().on('changeColor', function(ev){
   			var color = ev.color.toHex();
-  			console.log(color)
+  			var bodyStyle = $('body')[0].style;
   			$('.'+$(this).attr('id')).val(color);
+  			if($(this).attr('id') == 'background') {
+  				bodyStyle.backgroundColor = color;
+  			}
+
+  			if($(this).attr('id') == 'top') {
+  				$('.navbar-inverse .navbar-inner').css('background-color', color);
+  				$('.navbar-inverse .navbar-inner').css('background-image', 'none');
+  				$('.navbar-inverse .navbar-inner').css('border-color', color);
+  			}
+
+  			if($(this).attr('id') == 'footer') {
+  				$('footer').css('background-color', color);
+  			}
 	});
 
 }).call(this);
